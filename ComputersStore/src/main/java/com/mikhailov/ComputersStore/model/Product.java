c310f57e27c15d63b6c5cb422de08c875d41f381
Mikhailov Pavel <mikhailov.pi@yandex.ru>
2023-06-01 15:31:45 +0500

    crete model and schema.sql

---
+++ ComputersStore/src/main/java/com/mikhailov/ComputersStore/model/Product.java
@@ -0,0 +1,24 @@
+package com.mikhailov.ComputersStore.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Product {
+
+    Long id;
+
+    String numberSerial;
+
+    String manufacturer;
+
+    Integer price;
+
+    Integer quantity;
+}
