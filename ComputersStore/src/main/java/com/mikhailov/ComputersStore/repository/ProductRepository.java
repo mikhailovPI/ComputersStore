589e6362a2dd03cb407370f5237a82cc45cb678e
Mikhailov Pavel <mikhailov.pi@yandex.ru>
2023-06-04 13:46:24 +0500

    Style ProductRepository

--- ComputersStore/src/main/java/com/mikhailov/ComputersStore/repository/ProductRepository.java
+++ ComputersStore/src/main/java/com/mikhailov/ComputersStore/repository/ProductRepository.java
@@ -2,9 +2,6 @@ package com.mikhailov.ComputersStore.repository;
 
 import com.mikhailov.ComputersStore.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
